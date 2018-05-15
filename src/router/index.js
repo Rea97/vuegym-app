773addef6e5cf9a607fd4bb90ccd38151405561d
Oziel Martinez <ozielrea97@hotmail.com>
2018-05-15 18:32:58 -0500

    Add navigation guards to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import store from '@/vuex/store';
 import Landing from '@/pages/Landing';
 import SignUp from '@/pages/SignUp';
 import Home from '@/pages/Home';
 
 Vue.use(Router);
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/');
+};
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/login');
+};
 
 export default new Router({
   routes: [
@@ -17,11 +34,13 @@ export default new Router({
       path: '/signup',
       name: 'SignUp',
       component: SignUp,
+      beforeEnter: ifNotAuthenticated,
     },
     {
       path: '/home',
       name: 'Home',
       component: Home,
+      beforeEnter: ifAuthenticated,
     },
   ],
 });

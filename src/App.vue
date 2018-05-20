421808b44022db3ac22b2a398d735450c6121ed6
Oziel Martinez <ozielrea97@hotmail.com>
2018-05-20 17:49:54 -0500

    Add interceptor to axios to handle 401 responses

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,28 @@
 </template>
 
 <script>
+import axios from 'axios';
+import { mapActions } from 'vuex';
 import AppNavbar from './components/Navbar';
 
 export default {
   name: 'App',
   components: {
     AppNavbar,
+  },
+  methods: mapActions(['logOut']),
+  created() {
+    // eslint-disable-next-line no-unused-vars
+    axios.interceptors.response.use(undefined, err => new Promise((resolve, reject) => {
+      // eslint-disable-next-line no-underscore-dangle
+      if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+        // eslint-disable-next-line no-unused-vars
+        this.logOut().then((response) => {
+          this.$router.push('/signin');
+        });
+      }
+      throw err;
+    }));
   },
   computed: {
     shouldShowNavbar() {
